4a72b32a7302f1a50289d84f02b2dd96f432c6d7
Grain_Kitty <135233855+Grain-Kitty@users.noreply.github.com>
2025-01-03 01:29:59 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -112,7 +112,7 @@ onMounted(() => {
   const styleTitle1 = "font-size: 20px;font-weight: 600;color: rgb(244,167,89);";
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
-  const title1 = "無名の主页";
+  const title1 = "由imsyy强力驱动";
   const title2 = `
  _____ __  __  _______     ____     __
 |_   _|  \\/  |/ ____\\ \\   / /\\ \\   / /
@@ -121,7 +121,9 @@ onMounted(() => {
  _| |_| |  | |____) |  | |      | |
 |_____|_|  |_|_____/   |_|      |_|`;
   const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
+  console.group('本网站由imsyy强力驱动，部分代码有删改');
   console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent);
+  console.groupEnd();
 });
 
 onBeforeUnmount(() => {
